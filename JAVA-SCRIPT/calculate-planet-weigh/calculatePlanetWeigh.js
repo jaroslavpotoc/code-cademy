f3b8ada95eb5ee6850c6968e1053db29521f4be3
jaroslavpotoc <132379473+jaroslavpotoc@users.noreply.github.com>
2023-07-18 20:10:53 +0200

    Code cademy - new project

---
+++ JAVA-SCRIPT/calculate-planet-weigh/calculatePlanetWeigh.js
@@ -0,0 +1,18 @@
+// Write your function here:
+const calculateWeight = (earthWeight, planet) => {
+    if (planet === 'Mercury') {
+        return earthWeight * 0.378;
+    } else if (planet === 'Venus') {
+        return earthWeight * 0.907;
+    } else if (planet === 'Mars') {
+        return earthWeight * 0.377;
+    } else if (planet === 'Jupiter') {
+        return earthWeight * 2.36;
+    } else if (planet === 'Saturn') {
+        return earthWeight * 0.916;
+    } else {
+        return 'Invalid Planet Entry. Try: Mercury, Venus, Mars, Jupiter, or Saturn.';
+    }
+};
+
+console.log(calculateWeight(5.972e24, 'Mercury'));
