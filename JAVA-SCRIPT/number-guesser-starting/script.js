d04aa16c2fefcf2499c0827301a170df3ecca710
jaroslavpotoc <132379473+jaroslavpotoc@users.noreply.github.com>
2023-07-19 16:47:15 +0200

    Code cademy - new project

--- JAVA-SCRIPT/number-guesser-starting/script.js
+++ JAVA-SCRIPT/number-guesser-starting/script.js
@@ -9,9 +9,16 @@ const generateTarget = () => {
 
 console.log(generateTarget());
 
+
 const compareGuesses = (humanGuess, computerGuess, targetGuess) => {
     const humanDifference = Math.abs(targetGuess - humanGuess);
     const computerDifference = Math.abs(targetGuess - computerGuess);
+
+    if (humanDifference >= 9) {
+        alert('Add number 0 - 10 !');
+    } else if (humanDifference < -1 ) {
+        alert('Add number 0 - 10 !');
+    }
 
     return humanDifference <= computerDifference;
 };
@@ -25,8 +32,3 @@ const updateScore = (winner) => {
 };
 
 const advanceRound = () => currentRoundNumber++;
-
-
-/* TODO Pravdepodobne ste vypočítali vzdialenosť od odhadu počítača k cieľu a od ľudského odhadu k cieľu. Presuňte to do samostatnej getAbsoluteDistance()funkcie, ktorá vezme dve čísla a vráti vzdialenosť, a potom ju použite vo svojej compareGuesses()funkcii.
-  TODO Pridajte funkcionalitu na kontrolu, či používateľ odhaduje medzi 0 a 9 a alert()používateľ, že jeho číslo je mimo rozsahu. +Pomocou tlačidiel a nie je možné nastaviť číslo mimo tohto rozsahu =, ale používatelia to môžu urobiť zadaním priamo do vstupného poľa.
-*/
