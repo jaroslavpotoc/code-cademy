6292789f5931573826e172d4c541bbea7d85e3c9
jaroslavpotoc <jaroslavpotocny@icloud.com>
2023-11-23 16:33:04 +0100

    some change

--- javaScript/api-http-request/film-finder/public/script.js
+++ javaScript/api-http-request/film-finder/public/script.js
@@ -23,7 +23,7 @@ const getMovies = async () => {
   const selectedGenre = getSelectedGenre();
   const discoverMovieEndpoint = '/discover/movie';
   const requestParams = `?api_key=${tmdbKey}&with_genres=${selectedGenre}`;
-  const urlToFetch = `${tmdbBaseUrl}${genreRequestEndpoint}${requestParams}`;
+  const urlToFetch = `${tmdbBaseUrl}${discoverMovieEndpoint}${requestParams}`;
 
   try {
     const response = await fetch(urlToFetch);
@@ -37,17 +37,35 @@ const getMovies = async () => {
   }
 };
 
-const getMovieInfo = (movie) => {
+const getMovieInfo = async (movie) => {
+const movieId = movie.id;
+const movieEndpoint = `/movie/${movieId}`;
+const requestParams = `?api_key=${tmdbKey}`;
+const urlToFetch = `${tmdbBaseUrl}${movieEndpoint}${requestParams}`;
 
+  try {
+    const response = await fetch(urlToFetch);
+    if (response.ok) {
+      const jsonResponse = await response.json()
+      const movieInfo = jsonResponse;
+      return movieInfo;
+    }
+  } catch(error) {
+      console.log(error);
+  }
 };
 
+
 // Gets a list of movies and ultimately displays the info of a random movie from the list
-const showRandomMovie = () => {
+const showRandomMovie = async () => {
   const movieInfo = document.getElementById('movieInfo');
   if (movieInfo.childNodes.length > 0) {
     clearCurrentMovie();
   };
-
+  const movies = await getMovies();
+  const randomMovie = getRandomMovie(movies);
+  const info = await getMovieInfo(randomMovie);
+  displayMovie(info);
 };
 
 getGenres().then(populateGenreDropdown);
